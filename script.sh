b87f87c234be88dc0d152edacc045520f73f8828
sheylabarrientos <sheylaaraujobarrientos@gmail.com>
2020-04-30 18:55:01 -0300

    Inclusão de mensagem motivacional

--- script.sh
+++ script.sh
@@ -4,75 +4,7 @@
 
 #_____01______02________________________________________
 
-VAR="Shell Script é demais"
+VAR="Vou ter muito sucesso na programação! Sororidade =)"
 
 echo "$VAR"
-
-#____________________03_________________________________
-
-HOSTNAME=$(hostname)
-
-echo "Este script está rodando no computador: $HOSTNAME"
-
-#_________________________04_____________________________
-
-FILE="/ravena/casa/script.sh"
-
-if [ -e $FILE ]
-then
-    echo "O caminho $FILE está habilidado! "
-elif [ -w $FILE ]
-then
-    echo "Você tem permissão para editar $FILE"
-else
-    echo "Você NÃO foi autorizado a editar $FILE"
-fi
-
-#________________________________05_______________________
-
-arquivo=/home/ravena/lista.txt
-
-for nomes in `cat $arquivo`
-do 
-    echo $nomes
-done
-
-#_________________________________________06_______________
-
-echo "Digite o caminho de um arquivo ou diretorio:"
-
-read FILE
-
-if [ -f $FILE ]
-then
-    echo "o $FILE é um arquivo comum"
-
-elif [ -d $FILE ]
-then
-    echo "o $FILE é um diretorio"
-else
-    echo "é alguma outra coisa"
-fi
-
-ls -l $FILE
-
-#_________________07______________________________________________
-
-
-FILE=$1
-
-if [ -f $FILE ]
-then
-    echo "o $FILE é um arquivo comum"
-
-elif [ -d $FILE ]
-then
-    echo "o $FILE é um diretorio"
-else
-    echo "é alguma outra coisa"
-fi
-
-ls -l $FILE
-
-
 
