2affb25aeae55257157d569dd37247573df92707
sheylabarrientos <sheylaaraujobarrientos@gmail.com>
2020-05-12 18:26:59 -0300

    5angulosInternosTriangulo

---
+++ exercises/4_1/5angulosInternosTriangulo.js
@@ -0,0 +1,12 @@
+let ladoa = 10;
+let ladob = 9;
+let ladoc = 2;
+
+ladosDeUmTriangulo = 180;
+
+if (ladoa * ladob * ladoc === ladosDeUmTriangulo){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
